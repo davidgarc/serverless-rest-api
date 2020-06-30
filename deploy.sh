4b0954c6ab638c8368bb1e38f50283fd8c328656
David Garcia <david.tele07@gmail.com>
2020-06-30 02:52:34 -0500

    demo app working

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,15 @@ find . -name template.yaml | while read -r fname; do
   templateName="${fname}"
   packagedName="packaged.yaml"
   outputFileName="${templateName/template.yaml/$packagedName}"
-  echo "Packaging template: ${templateName}"
-  sam package --template-file "$templateName" --output-template-file "$outputFileName" --s3-bucket "orders-demo-configs"
+  echo "Packaging template ${templateName} into ${outputFileName}:"
+  sam package --template-file "$templateName" --output-template-file "$outputFileName" --s3-bucket "orders-demo-configs-$1"
 done
 
 echo ""
 echo "Coping to s3 templates"
-aws s3 cp --recursive --exclude "*template.yaml" templates "s3://orders-demo-configs/templates/"
+aws s3 cp --recursive --exclude "*template.yaml" templates "s3://orders-demo-configs-$1/templates/"
 
 echo ""
 echo "Deploying application"
 sam deploy --template-file packaged.yaml --stack-name "orders-demo-app-$1" --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --parameter-overrides "Environment=$1"
 echo "Done deploying $1 application"
-
